09589f469b8646e0ab7f2c67c2158b612fb907a0
Jeancasoto <jeancasoto@unitec.edu>
2017-11-17 18:45:48 -0600

    Casi finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include "Persona.h"
+#include "Poder.h"
+#include "AirBender.h"
+#include "FireBender.h"
+#include "WaterBender.h"
+#include "EarthBender.h"
+#include "NonBender.h"
+#include "Ofensivo.h"
+#include "Defensivo.h"
+#include "Curativo.h"
+#include "Invocacion.h"
 
 using namespace std;
 
@@ -13,43 +25,75 @@ int main(){
 
     char resp='s';
     while(resp=='s' || resp=='S'){
+        vector<Persona*> personas;
         char r;
         cout<<"--------ADMINISTRADOR PERSONAJES DE AVATAR------"<<endl<<endl;
         cout<<"a)Agregar \nb)Eliminar \nc)Listar \nd)Salir"<<endl<<endl;
         cin>>r;
         switch(r){
-            case 'a':{
+             
+            case 'a':{//switch de opciones
                 char maestro;
-                cout<<"a)Agregar maestro \nb)Agregar normalmente"<<endl;
+                cout<<"A)Agregar maestro \nB)Agregar normalmente"<<endl;
                 cin>>maestro;
-                switch(maestro){
-                    case 'a':{
-                        
-
-                    }
-                    break;
-                    //fin case a de maestro
-                    case 'b':{
-
-
-                    }
-                    break;
-                    //fin case b de maestro
+                if(maestro=='A'){
 
                 }
+                if(maestro=='B'){
+                     string cadena="";
+                    string nacion;
+                    string nombre;
+                    string edad;
+                    string sexo;
+                    string trabajo;
+                    string fuerza;
+                    string velocidad;
 
-                
+                    cout<<"Ingrese nacion"<<endl;
+                    cin>>nacion;
+                    cadena+=nacion;
+                    cout<<"Ingrese nombre"<<endl;
+                    cin>>nombre;
+                    cadena+=nombre;
+                    cout<<"Ingrese edad"<<endl;
+                    cin>>edad;
+                    cadena+=edad;
+                    cout<<"Ingrese sexo"<<endl;
+                    cin>>sexo;
+                    cadena+=sexo;
+                    cout<<"Ingrese trabajo"<<endl;
+                    cin>>trabajo;
+                    cadena+=trabajo;
+                    cout<<"Ingrese fuerza"<<endl;
+                    cin>>fuerza;
+                    cadena+=fuerza;
+                    cout<<"Ingrese velocidad"<<endl;
+                    cin>>velocidad;
+                    cadena+=velocidad;
+
+                   Persona* non= new NonBender(nacion, nombre, edad, sexo, trabajo, fuerza, velocidad );
+                    
+                  
+                   ofstream file ("Texto.txt");
+                    if(file.is_open()){
+                        file<<cadena;
+                    }//fin if
+                 file.close();   
+
+
+                  
+                }
             }
             break;
             //fin case a
             case 'b':{
-
-
+                cout<<"CASOB"<<endl;
+                 
             }
             break;
             //fin case b
             case 'c':{
-
+                 cout<<"CASOC"<<endl;
 
             }
             break;
